ba1b7ad532eaf2032ab907ae0a128b477f25a2fc
MakHurst <makhurst.tech@gmail.com>
2022-11-20 01:31:20 -0800

    code update

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -6,4 +6,33 @@ let turn = PLAYER_X;
 const boardState = Array(tiles.length);
 boardState.fill(null);
 
-console.log("hey");
+// Elements 
+const strike = document.getElementById('strike');
+const gameOverArea = document.getElementById("game-over-area");
+const gameOvertext = document.getElementById("game-over-text");
+const playAgain = document.getElementById("play-again");
+
+// Sounds 
+ const gameOverSound = new Audio(null);
+ const click = new Audio(null);
+
+ tiles.forEach((tile) => tile.addEventListener("click", tileClick));
+
+ function tileClick(event) {
+
+    if (gameOverArea.classList.contains('visible')) {
+        return;
+    }
+
+    const tile = event.target;
+    const tileNumber = tile.dataset.index;
+    if (tile.innerText != "") {
+        return;
+    }
+
+    if (turn === PLAYER_X) {
+        tile.innerText = PLAYER_X;
+        boardState[tileNumber - 1] = PLAYER_X;
+        turn = PLAYER_O;
+    }
+ }
